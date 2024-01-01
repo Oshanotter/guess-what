e7f2414eb73589f95d6dab162010d7b121bf775d
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2024-01-01 15:07:55 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -52,9 +52,10 @@ self.addEventListener('fetch', function(evt) {
 
 // Open the cache where the assets were stored and search for the requested resource. Notice that in case of no matching, the promise still resolves but it does with undefined as value.
 function fromCache(request) {
-  return caches.open(RUNTIME).then(function (cache) {
-    return cache.match(request);
-  });
+  caches.match(request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
 }
 
 // Update consists in opening the cache, performing a network request and storing the new response data.
