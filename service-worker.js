7a061c99ea2ae8102d8554cbb332a4fdb5d9569d
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2024-01-01 11:20:20 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,3 @@
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
-
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
 const PRECACHE = 'precache-v2';
 const RUNTIME = 'runtime';
 
@@ -22,58 +6,45 @@ const PRECACHE_URLS = [
   'index.html',
   './', // Alias for index.html
   'styles.css',
-  'script.js', 
+  'script.js',
   'manifest.json',
   'icon.png'
 ];
 
-// The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(PRECACHE)
       .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
+      return Promise.all(
+        cacheNames
+          .filter(cacheName => cacheName.startsWith('precache-') && cacheName !== PRECACHE)
+          .map(cacheToDelete => caches.delete(cacheToDelete))
+      );
     }).then(() => self.clients.claim())
   );
 });
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-// On fetch, use cache but update the entry with the latest contents from the server.
 self.addEventListener('fetch', function(evt) {
   console.log('The service worker is serving the asset.');
-  // You can use respondWith() to answer ASAP…
   evt.respondWith(fromCache(evt.request));
-  // ...and waitUntil() to prevent the worker to be killed until the cache is updated.
   evt.waitUntil(
     update(evt.request)
-    // Finally, send a message to the client to inform it about the resource is up to date.
-    .then(refresh)
+      .then(refresh)
   );
 });
 
-// Open the cache where the assets were stored and search for the requested resource. Notice that in case of no matching, the promise still resolves but it does with undefined as value.
 function fromCache(request) {
   return caches.open(RUNTIME).then(function (cache) {
     return cache.match(request);
   });
 }
 
-// Update consists in opening the cache, performing a network request and storing the new response data.
 function update(request) {
   return caches.open(RUNTIME).then(function (cache) {
     return fetch(request).then(function (response) {
@@ -84,19 +55,14 @@ function update(request) {
   });
 }
 
-// Sends a message to the clients.
 function refresh(response) {
   return self.clients.matchAll().then(function (clients) {
     clients.forEach(function (client) {
-    // Encode which resource has been updated. By including the ETag the client can check if the content has changed.
       var message = {
         type: 'refresh',
         url: response.url,
-        // Notice not all servers return the ETag header. If this is not provided you should use other cache headers or rely on your own means to check if the content has changed.
         eTag: response.headers.get('ETag')
       };
-
-      // Tell the client about the update.
       client.postMessage(JSON.stringify(message));
     });
   });
