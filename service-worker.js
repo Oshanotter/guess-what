e5de871f8365ca2c8886522a661b6d8d64bbcd46
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2024-01-01 15:20:09 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,75 @@
-const CACHE_NAME = 'my-cache-v1';
+const PRECACHE = 'precache-v2';
+const RUNTIME = 'runtime';
 
-const urlsToCache = [
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
   'index.html',
   './', // Alias for index.html
   'astyles.css',
-  'main.css',
-  'demo.js'
+  'script.js', 
+  'manifest.json',
+  'icon.png'
 ];
 
+// The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Try to fetch a fresh copy from the network
-        return fetch(event.request)
-          .then(response => {
-            // Update the cache with the new response
-            return caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, response.clone());
-                return response;
-              });
-          })
-          .catch(() => {
-            // If fetching from the network fails, return the cached response
-            return response || caches.match('offline.html'); // Provide a fallback page if needed
-          });
-      })
-  );
-});
-
+// The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', event => {
-  // Remove outdated caches
+  const currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
   );
 });
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+// On fetch, use cache but update the entry with the latest contents from the server.
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.');
+  // You can use respondWith() to answer ASAP…
+  evt.respondWith(fromCache(evt.request));
+  // ...and waitUntil() to prevent the worker to be killed until the cache is updated.
+  evt.waitUntil(
+    update(evt.request)
+    // Finally, send a message to the client to inform it about the resource is up to date.
+    .then(refresh)
+  );
+});
+
+// Open the cache where the assets were stored and search for the requested resource. Notice that in case of no matching, the promise still resolves but it does with undefined as value.
+function fromCache(request) {
+  return caches.open(RUNTIME).then(function (cache) {
+    return cache.match(request);
+  });
+}
+
+// Update consists in opening the cache, performing a network request and storing the new response data.
+function update(request) {
+  return caches.open(RUNTIME).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response.clone()).then(function () {
+        return response;
+      });
+    });
+  });
+}
+
+// Sends a message to the clients.
+function refresh(response) {
+  client.postMessage("update");
+  console.log("update me")
+}
