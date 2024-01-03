076216a4e3d8cd706f5426697fd8cfa9ff36dbec
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2024-01-03 08:09:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -332,7 +332,7 @@ function installPrompt() {
 		// prompt to install on iOS
 		theText.innerHTML = "<p>The experience is better with the App!</p><p>Download it by tapping the share button <img src='https://i.imgur.com/eUbPxhg.png' style='display: inline; height: 10px; width: auto;' alt='img-mail' /> below, then tap on 'Add to Home Screen'</p>"
 		
-	}else{
+	}else if (!isiOS()){
 		// prompt to install on Android
 		theText.innerText = "Please install the PWA for Android"
 	}
