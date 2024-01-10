2006c67a4861817a1e32faccccb96b09cba43d39
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2024-01-10 11:09:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -477,7 +477,8 @@ function buildGamePreview(title, description) {
     var top = document.createElement('div');
     top.style.height = "65%";
     top.className = "gamePreview";
-    //top.style.display = "flex";
+    top.style.display = "flex";
+    top.style.flexDirection = "column";
     top.style.justifyContent = "space-between";
     
     var bottom = document.createElement('div');
