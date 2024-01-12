e771571eb345e1b9fddc339d37ccd144b4601e1d
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2024-01-12 12:11:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -499,10 +499,7 @@ function buildGamePreview(title, description) {
     var bottom = document.createElement('div');
     bottom.style.height = bottomHeight
     bottom.className = "gamePreview";
-    bottom.style.display = "flex";
-    bottom.style.flexDirection = "column";
-    bottom.style.justifyContent = "center";
-    bottom.style.alignItems = "center";
+    bottom.style.marginTop = "10%";
 	
 	mainDiv.style.borderRadius = "2vmin";
 	timeOptionsDiv.style.borderRadius = "999px";
