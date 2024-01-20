66df8a6839a73fdc1b86d6d8d136ea0e3e564045
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2024-01-20 11:01:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -581,8 +581,14 @@ function addPlayButton() {
 function preventScroll(bool){
     if (bool){
         document.body.style.overflow = "hidden";
+	document.body.style.height = "100%";
+	document.documentElement.style.overflow = "hidden";
+	document.documentElement.style.height = "100%";
     }else{
         document.body.style.overflow = '';
+	document.body.style.height = '';
+	document.documentElement.style.overflow = '';
+	document.documentElement.style.height = '';
     }
 }
 
