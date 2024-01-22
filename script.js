26a07f2ea5ba3efcde0c353bb8859e649001771f
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2024-01-21 20:37:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -594,17 +594,16 @@ function preventScroll(bool){
 
 
 
-/*
+
 function toggleHeart(gameID) {
 	var theElement = document.getElementById(gameID);
 	var heartDiv = theElement.querySelector("div")
-	if (heartDiv.innerText == ♥) {
-		heartDiv.innerText = ♡;
+	if (heartDiv.innerText == "♥") {
+		heartDiv.innerText = "♡";
 	} else {
-		heartDiv.innerText = ♥;
+		heartDiv.innerText = "♥";
 	}
 }
-*/
 
 
 
