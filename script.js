7d03e15915ff36c9b5f1a75e2f307068d9ae03d3
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2024-02-03 17:39:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -580,6 +580,7 @@ function addPlayButton() {
 }
 
 function preventScroll(bool){
+	return;
     if (bool){
         document.body.style.overflow = "hidden";
 	/*document.body.style.height = "100%";*/
@@ -792,7 +793,6 @@ function createGame() {
 	var dictString = JSON.stringify(dict);
 	//storeUserCreatedGame(dictString, gameID);
 	generateUserCreatedGame(dict);
-	preventScroll(true);
 }
 
 function shareGame(id){
