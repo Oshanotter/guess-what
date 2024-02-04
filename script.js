7c73002ad7e08783632f54bf1b6be6a8507adb8e
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2024-02-04 16:37:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -631,9 +631,11 @@ function allowScroll(element) {
 		element.scrollTop = element.scrollHeight - element.clientHeight - 1;
             }
         });
-	alert('almost done')
+
+	setTimeout( function(){
 	// set topscroll to 1 immediately so that scrolling up doesn't casue the whole screen to move
 	element.scrollTop = 1;
+	}, 1000);
 	
 }
 
