30dddebbc7216401815a9366e21140f7f90cd8b9
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2024-02-20 22:20:22 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,12 @@ const PRECACHE_URLS = [
   'icons/masked.png',
   'game-sets/pokemon.json',
   'game-sets/test.json',
-  'icons/menu-bar/allGames.png',
-  'icons/menu-bar/favorites.png',
-  'icons/menu-bar/create.png',
-  'icons/menu-bar/settings.png'
+  'icons/general/allGames.png',
+  'icons/general/favorites.png',
+  'icons/general/create.png',
+  'icons/general/settings.png',
+  'icons/general/share.png',
+  'icons/general/import.png'
 ];
 
 // The install handler takes care of precaching the resources we always need.
