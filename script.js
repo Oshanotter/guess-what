127f96f54c7ecb540c8cacba4c9ad59b4af4bcf0
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2024-02-22 14:15:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1122,8 +1122,8 @@ async function uploadUserCreatedGame(data, errorCount=0){
 		var url = 'https://tinyurl.com/api-create.php?url=' + location.href + '?s=' + data;
 		const response = await fetch(url);
 	  	const tinyURL = await response.text()
-		var split = tinyURL.split("/");
-		var id = split[split.length - 1];
+		var parts = tinyURL.split("/");
+		var id = parts[parts.length - 1];
 		displayPopup("Your Shareable Code:<br><br><b style='font-size: 150%;'>" + id + "</b>", "Close", "Copy Code", function (){
 			// Create a temporary input element to copy the code to the clipboard
 		      	var tempInput = document.createElement('input');
