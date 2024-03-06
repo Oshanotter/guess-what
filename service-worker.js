65923ddcaac97c72b82ea5004763a91910cb1216
Max Forst <103145365+Oshanotter@users.noreply.github.com>
2024-03-05 21:53:29 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,18 @@
 const PRECACHE = 'precache';
 
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
+// cache the main files needed for the webpage
+const mainFiles = [
   'index.html',
   './', // Alias for index.html
   'styles.css',
   'script.js', 
   'manifest.json',
   'icons/full.png',
-  'icons/masked.png',
-  'game-sets/pokemon.json',
-  'game-sets/test.json',
+  'icons/masked.png'
+];
+
+// cache the icons for the menu bar and other stuff
+const generalIcons = [
   'icons/general/allGames.png',
   'icons/general/favorites.png',
   'icons/general/create.png',
@@ -24,11 +26,30 @@ const PRECACHE_URLS = [
   'icons/general/arrow-replay.svg',
   'icons/general/dropdown-menu.svg',
   'icons/general/heart-fill.svg',
-  'icons/general/heart-outline.svg',
+  'icons/general/heart-outline.svg'
+];
+
+// cache the audio files
+const audioFiles = [
   'audio/correct.mp3',
   'audio/pass.mp3',
   'audio/count.mp3'
 ];
+
+// cache the json files for individual games
+const gameJSONs = [
+  'game-sets/pokemon.json',
+  'game-sets/test.json'
+];
+
+// cache the icon files for individual games
+const gameIcons = [
+  
+];
+
+
+// combine all of the cached pages into one list
+const PRECACHE_URLS = [...mainFiles, ...generalIcons, ...audioFiles, ...gameJSONs, ...gameIcons];
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
@@ -61,8 +82,6 @@ self.addEventListener('fetch', function(evt) {
   // ...and waitUntil() to prevent the worker to be killed until the cache is updated.
   evt.waitUntil(
     update(evt.request)
-    // Finally, send a message to the client to inform it about the resource is up to date.
-    //.then(refresh)
   );
 });
 
@@ -80,24 +99,6 @@ function update(request) {
       return cache.put(request, response.clone()).then(function () {
         return response;
       });
-    });
-  });
-}
-
-// Sends a message to the clients.
-function refresh(response) {
-  return self.clients.matchAll().then(function (clients) {
-    clients.forEach(function (client) {
-    // Encode which resource has been updated. By including the ETag the client can check if the content has changed.
-      var message = {
-        type: 'refresh',
-        url: response.url,
-        // Notice not all servers return the ETag header. If this is not provided you should use other cache headers or rely on your own means to check if the content has changed.
-        eTag: response.headers.get('ETag')
-      };
-
-      // Tell the client about the update.
-      client.postMessage(JSON.stringify(message));
     });
   });
 }
